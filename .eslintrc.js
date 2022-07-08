7cf3aa0dcfa5aa0cca1abb381211fb6b37ff19f4
yusongH <549962989@qq.com>
2022-07-08 23:18:19 +0800

    feat: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,10 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {}
+  rules: {
+    // "off" or 0 - 关闭规则
+    // "warn" or 1 - 将规则视为一个警告
+    // "error" or 2 - 将规则视为一个错误
+    eqeqeq: 2 // 强制使用 === 和 !==
+  }
 }
